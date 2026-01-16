bc63b45d3ae20e5660f605972f2628e3ecd51f87
pedroairtonz <pedroairton@zaite.com.br>
2026-01-16 16:50:35 -0300

    dashboard arrumado

--- src/app/features/admin/dashboard/dashboard.component.ts
+++ src/app/features/admin/dashboard/dashboard.component.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NewsService } from '../../../core/services/news.service';
+import { News } from '../../../core/models/news.model';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../../../core/services/auth.service';
 
+export interface DashboardItensSuperAdmin{
+  total_news: number,
+  published_news: number,
+  draft_news: number,
+  total_authors: number,
+  total_categories: number,
+  total_tags: number,
+  total_views: number,
+  recent_news: News[]
+}
+export interface DashboardItensAutor{
+  my_total_news: number,
+  my_published_news: number,
+  my_draft_news: number,
+  my_total_views: number,
+  recent_my_news: News[]
+}
 @Component({
   selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
 
+export class DashboardComponent {
+  isSuperAdmin$: boolean = false
+  private newsService = inject(NewsService)
+  private authService = inject(AuthService)
+  private toastr = inject(ToastrService)
+  public dashboard: DashboardItensSuperAdmin | DashboardItensAutor = {} as DashboardItensSuperAdmin | DashboardItensAutor
+
+  ngOnInit(){
+    this.checkSuperAdmin()
+    this.newsService.getDashboardStats().subscribe({
+      next: (response) => {
+        this.dashboard = response
+
+      },
+      error: (error) => {
+        console.error(error)
+        this.toastr.error('Erro ao carregar estatísticas do painel')
+      }
+    })
+  }
+  checkSuperAdmin() {
+    this.authService.isSuperAdmin().subscribe({
+      next: (response: any) => {
+        this.isSuperAdmin$ = response;
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
 }
